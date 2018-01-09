8b06e017de2009854942250170edd42fec20d568
Yan Zhang <yanzh@microsoft.com>
2018-01-09 17:22:46 +0800

    specific telemetry items

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@
 import * as fs from "fs";
 import * as unzip from "unzip";
 import * as vscode from "vscode";
-import { TelemetryWrapper } from "vscode-extension-telemetry-wrapper";
+import { TelemetryWrapper, Transaction } from "vscode-extension-telemetry-wrapper";
 import { DependencyManager, IDependencyQuickPickItem } from "./DependencyManager";
 import { Utils } from "./Utils";
 import { VSCodeUI } from "./VSCodeUI";
@@ -20,21 +20,28 @@ export async function activate(context: vscode.ExtensionContext): Promise<void> 
 
     ["maven", "gradle"].forEach((projectType: string) => {
         context.subscriptions.push(
-            TelemetryWrapper.registerCommand(`spring.initializr.${projectType}`, () => {
-                return async () => await generateProjectRoutine(projectType);
+            TelemetryWrapper.registerCommand(`spring.initializr.${projectType}`, (t: Transaction) => {
+                return async () => await generateProjectRoutine(projectType, t);
             })
         );
     });
 }
 
-async function generateProjectRoutine(projectType: string): Promise<void> {
+async function generateProjectRoutine(projectType: string, transaction?: Transaction): Promise<void> {
+    transaction.customProperties.finishedSteps = [];
+    // Step: Group Id
     const groupId: string = await VSCodeUI.getFromInputBox({ prompt: STEP1_MESSAGE, placeHolder: "e.g. com.example" });
     if (groupId === undefined) { return; }
+    transaction.customProperties.finishedSteps.push("GroupId");
+
+    // Step: Artifact Id
     const artifactId: string = await VSCodeUI.getFromInputBox({ prompt: STEP2_MESSAGE, placeHolder: "e.g. demo" });
     if (artifactId === undefined) { return; }
+    transaction.customProperties.finishedSteps.push("ArtifactId");
+
+    // Step: Dependencies
     let current: IDependencyQuickPickItem = null;
     const manager: DependencyManager = new DependencyManager();
-
     do {
         current = await vscode.window.showQuickPick(
             manager.getQuickPickItems(), { ignoreFocusOut: true, placeHolder: STEP3_MESSAGE }
@@ -42,35 +49,40 @@ async function generateProjectRoutine(projectType: string): Promise<void> {
         if (current && current.itemType === "dependency") {
             manager.toggleDependency(current.id);
         }
-    } while (current && current.itemType !== "command");
-    if (current) {
-        // Choose target folder
-        const outputUri: vscode.Uri = await VSCodeUI.openDialogForFolder({ openLabel: "Generate into this folder" });
-        if (!outputUri) { return; }
-        // Download & Unzip
-        await vscode.window.withProgress({ location: vscode.ProgressLocation.Window }, (p: vscode.Progress<{ message?: string }>) => new Promise<void>(
-            async (resolve: () => void, _reject: (e: Error) => void): Promise<void> => {
-                p.report({ message: "Downloading zip package..." });
-                const targetUrl: string = `https://start.spring.io/starter.zip?type=${projectType}-project&groupId=${groupId}&artifactId=${artifactId}&style=${current.id}`;
-                const filepath: string = await Utils.downloadFile(targetUrl);
+    } while (current && current.itemType === "dependency");
+    if (!current) { return; }
+    transaction.customProperties.finishedSteps.push("Dependencies");
+    transaction.customProperties.depsType = current.itemType;
+    transaction.customProperties.dependencies = current.id;
 
-                p.report({ message: "Starting to unzip..." });
-                fs.createReadStream(filepath).pipe(unzip.Extract({ path: outputUri.fsPath })).on("close", () => {
-                    manager.updateLastUsedDependencies(current);
-                    resolve();
-                }).on("error", (err: Error) => {
-                    vscode.window.showErrorMessage(err.message);
-                    resolve();
-                });
-            }
-        ));
+    // Step: Choose target folder
+    const outputUri: vscode.Uri = await VSCodeUI.openDialogForFolder({ openLabel: "Generate into this folder" });
+    if (!outputUri) { return; }
+    transaction.customProperties.finishedSteps.push("TargetFolder");
 
-        //Open in new window
-        const choice: string = await vscode.window.showInformationMessage(`Successfully generated. Location: ${outputUri.fsPath}`, "Open it");
-        if (choice === "Open it") {
-            const hasOpenFolder: boolean = (vscode.workspace.workspaceFolders !== undefined);
-            vscode.commands.executeCommand("vscode.openFolder", outputUri, hasOpenFolder);
+    // Step: Download & Unzip
+    await vscode.window.withProgress({ location: vscode.ProgressLocation.Window }, (p: vscode.Progress<{ message?: string }>) => new Promise<void>(
+        async (resolve: () => void, _reject: (e: Error) => void): Promise<void> => {
+            p.report({ message: "Downloading zip package..." });
+            const targetUrl: string = `https://start.spring.io/starter.zip?type=${projectType}-project&groupId=${groupId}&artifactId=${artifactId}&style=${current.id}`;
+            const filepath: string = await Utils.downloadFile(targetUrl);
+
+            p.report({ message: "Starting to unzip..." });
+            fs.createReadStream(filepath).pipe(unzip.Extract({ path: outputUri.fsPath })).on("close", () => {
+                manager.updateLastUsedDependencies(current);
+                resolve();
+            }).on("error", (err: Error) => {
+                vscode.window.showErrorMessage(err.message);
+                resolve();
+            });
         }
+    ));
+    transaction.customProperties.finishedSteps.push("DownloadUnzip");
+    //Open in new window
+    const choice: string = await vscode.window.showInformationMessage(`Successfully generated. Location: ${outputUri.fsPath}`, "Open it");
+    if (choice === "Open it") {
+        const hasOpenFolder: boolean = (vscode.workspace.workspaceFolders !== undefined);
+        vscode.commands.executeCommand("vscode.openFolder", outputUri, hasOpenFolder);
     }
 }
 
