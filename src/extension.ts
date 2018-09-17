1db173549835afa42cc50c31ac26e875af515897
Yan Zhang <Eskibear@users.noreply.github.com>
2018-09-17 15:18:28 +0800

    Update wrapper to apply new API. (#77)
    * Update wrapper to apply new API.
    
    * Fix format.

--- src/extension.ts
+++ src/extension.ts
@@ -3,25 +3,28 @@
 
 "use strict";
 import * as vscode from "vscode";
-import { TelemetryWrapper } from "vscode-extension-telemetry-wrapper";
+import { initializeFromJsonFile, instrumentOperation, TelemetryWrapper } from "vscode-extension-telemetry-wrapper";
 import { Routines } from "./Routines";
 import { Utils } from "./Utils";
 import { VSCodeUI } from "./VSCodeUI";
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
+    await initializeFromJsonFile(context.asAbsolutePath("./package.json"));
+    await instrumentOperation("activation", initializeExtension)(context);
+}
+
+async function initializeExtension(_operationId: string, context: vscode.ExtensionContext): Promise<void> {
     await Utils.loadPackageInfo(context);
     await TelemetryWrapper.initilizeFromJsonFile(context.asAbsolutePath("package.json"));
 
     ProjectTypes.all().forEach((projectType) => {
-        context.subscriptions.push(
-            TelemetryWrapper.registerCommand(`spring.initializr.${projectType.value}`, async () => await Routines.GenerateProject.run(projectType.value))
-        );
+        context.subscriptions.push(instrumentAndRegisterCommand(`spring.initializr.${projectType.value}`, async () => await Routines.GenerateProject.run(projectType.value)));
     });
 
-    context.subscriptions.push(TelemetryWrapper.registerCommand("spring.initializr.editStarters", async (entry: vscode.Uri) => await Routines.EditStarters.run(entry)));
+    context.subscriptions.push(instrumentAndRegisterCommand("spring.initializr.editStarters", async (entry: vscode.Uri) => await Routines.EditStarters.run(entry)));
 
-    context.subscriptions.push(TelemetryWrapper.registerCommand("spring.initializr.generate", async () => {
-        const projectType: ProjectType = await VSCodeUI.getQuickPick(ProjectTypes.all(), item => item.title, null, null, {placeHolder: "Select project type."});
+    context.subscriptions.push(instrumentAndRegisterCommand("spring.initializr.generate", async () => {
+        const projectType: ProjectType = await VSCodeUI.getQuickPick(ProjectTypes.all(), item => item.title, null, null, { placeHolder: "Select project type." });
         await vscode.commands.executeCommand(`spring.initializr.${projectType.value}`);
     }));
 }
@@ -36,7 +39,7 @@ type ProjectType = {
 };
 
 namespace ProjectTypes {
-    export const MAVEN : ProjectType = {
+    export const MAVEN: ProjectType = {
         title: "Maven Project",
         value: "maven-project"
     };
@@ -47,4 +50,9 @@ namespace ProjectTypes {
     export function all(): ProjectType[] {
         return [MAVEN, GRADLE];
     }
+}
+
+function instrumentAndRegisterCommand(name: string, cb: (...args: any[]) => any): vscode.Disposable {
+    const instrumented: (...args: any[]) => any = instrumentOperation(name, async (_operationId, myargs) => await cb(myargs));
+    return vscode.commands.registerCommand(name, instrumented);
 }
