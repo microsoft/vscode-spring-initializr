972f7ee31133bf54b62e6f6b8f5038110a2fdfa9
Yan Zhang <yanzh@microsoft.com>
2020-07-30 10:59:21 +0800

    can compare both legacy version format and SemVer
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/Utils/VersionHelper.ts
+++ src/Utils/VersionHelper.ts
@@ -1,10 +1,18 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
+/**
+ * Support both Legacy(v2.1) and Semver(v2.2)
+ * See: https://spring.io/blog/2020/04/30/updates-to-spring-versions
+ */
 const strictRange: RegExp = /\[(.*),(.*)\]/;
 const halfopenRightRange: RegExp = /\[(.*),(.*)\)/;
 const halfopenLeftRange: RegExp = /\((.*),(.*)\]/;
-const qualifiers: string[] = ["M", "RC", "BUILD-SNAPSHOT", "RELEASE"];
+
+/**
+ * Legacy: "BUILD-SNAPSHOT" is changed to SemVer: "SNAPSHOT"
+ */
+const qualifiers: string[] = ["M", "RC", "BUILD-SNAPSHOT", "SNAPSHOT", "RELEASE"];
 
 export function matchRange(version: string, range: string): boolean {
     const strictMatchGrp: RegExpMatchArray = range.match(strictRange);
@@ -29,8 +37,19 @@ export function matchRange(version: string, range: string): boolean {
 export function compareVersions(a: string, b: string): number {
     let result: number;
 
-    const versionA: string[] = a.split(".");
-    const versionB: string[] = b.split(".");
+    // Legacy: "major.minor.patch.qualifier"
+    // SemVer: "major.minor.patch-qualifier"
+    const versionA: string[] = a.split(/\.|-/g);
+    const versionB: string[] = b.split(/\.|-/g);
+
+    // workaround for qualifier "BUILD-SNAPSHOT" in v2.1
+    if (versionA.length === 5) {
+        versionA[3] = `${versionA[3]}-${versionA[4]}`;
+    }
+    if (versionB.length === 5) {
+        versionB[3] = `${versionB[3]}-${versionB[4]}`;
+    }
+
     for (let i: number = 0; i < 3; i += 1) {
         result = parseInt(versionA[i], 10) - parseInt(versionB[i], 10);
         if (result !== 0) {
