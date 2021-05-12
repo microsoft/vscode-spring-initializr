bba104168aac93457e28f4cbec5727ab3272c62e
Yan Zhang <yanzh@microsoft.com>
2021-05-12 14:06:50 +0800

    add license
    Signed-off-by: Yan Zhang <yanzh@microsoft.com>

--- src/Utils/fsHelper.ts
+++ src/Utils/fsHelper.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 import { FileType, Uri, workspace } from "vscode";
 
 export async function isDirectory(uri: Uri): Promise<boolean | undefined> {
